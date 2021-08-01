53bb0f57e5d8940cc63df7581f7ae9091954e653
Charles Lobe <lobecf@gmail.com>
2021-08-01 18:31:37 -0400

    commit tree 7d9becffe72acba91b58c5e48d50aafc877dee5c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData() {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+        // .then((res) => res.json())
+        // .then((data) => renderData(data))
+    })
+
+}
